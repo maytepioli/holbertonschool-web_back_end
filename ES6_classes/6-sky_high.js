90be50c59e127a03ac4cf431a59da16174db0915
maytepioli <maytepioli@gmail.com>
2024-11-21 19:40:53 -0300

    Implement a class named SkyHighBuilding that extends from Building

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  // eslint-disable-next-line constructor-super
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    return 'Evacuate slowly the NUMBER_OF_FLOORS floors.';
+  }
+}
