f5df6caa9590d96681526c21a66580b351cb3513
maytepioli <maytepioli@gmail.com>
2024-12-02 16:57:54 -0300

    return sum float

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Function that return a add of two float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function that return a add of two float
+    """
+    return a + b 
