1abdeb5feb53e83abe7bffab377ffd18bd96ec95
maytepioli <maytepioli@gmail.com>
2024-12-02 17:17:42 -0300

    pycodestyle

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-this fuction floor that takes a float n as an 
+this fuction floor that takes a float n as an
 argument and returns the floor of the float.
 """
 
 
 def floor(n: float) -> int:
     """
-    floor that takes a float n as an argument and returns the floor of the float.
+    floor that takes a float n as an argument and returns
+    the floor of the float.
     """
     return int(n)
