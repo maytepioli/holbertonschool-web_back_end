5ad71b558d86c7e9962877ac0f6fac75bbe68737
maytepioli <maytepioli@gmail.com>
2024-12-02 17:10:24 -0300

    concat string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Concatenate two strings and return them
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings and return them
+    """
+    return str1 + str2
