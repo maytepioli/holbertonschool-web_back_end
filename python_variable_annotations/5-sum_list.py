5aa62e5610d2ffa6c9596fde89b1322f60701154
maytepioli <maytepioli@gmail.com>
2024-12-04 09:40:43 -0300

    function that adds the elements of a list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Esta función recibe una lista de números y devuelve la suma de
+todos los elementos
+"""
+
+
+def sum_list(input_list: list) -> float:
+    """
+    Esta función recibe una lista de números y devuelve la suma de
+    todos los elementos
+    """
+    return sum(input_list)
