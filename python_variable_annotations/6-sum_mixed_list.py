be0d1e66c87e6a92b40a637a5c7ed4871d75c0d7
maytepioli <maytepioli@gmail.com>
2024-12-04 09:56:52 -0300

    fuction return sum floats

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Esta función recibe una lista de números enteros o flotantes, y devuelve
+    la suma de esos números como un número flotante.
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """
+    Esta función recibe una lista de números enteros o flotantes, y devuelve
+    la suma de esos números como un número flotante.
+    """
+    total = sum(mxd_lst)
+    return float(total)
