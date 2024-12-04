d634c740ec0baeb87615891e3b5bd5a32fdc5096
maytepioli <maytepioli@gmail.com>
2024-12-04 09:27:03 -0300

    Convert a float number to its text string representation

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Convert a float number to its text string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float number to its text string representation.
+    """
+    return str(n)
