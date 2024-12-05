106d428ed4788d4065606a8d53c9b5455d52090c
maytepioli <maytepioli@gmail.com>
2024-12-05 10:34:53 -0300

    fuction return tupla

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -8,4 +8,7 @@ from typing import Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
-    return (k, float(v** 2))
+    """
+    funcion retorna una tupla
+    """
+    return (k, float(v ** 2))
