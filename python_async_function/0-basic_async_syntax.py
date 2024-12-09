3a339fc2407e8139b5465152859641903fea0b36
maytepioli <maytepioli@gmail.com>
2024-12-08 23:38:46 -0300

    fuction asynchronous

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """
-funcion asincrónica
+This file contains a function asynchronous
 """
-import random
 import asyncio
+import random
 
 
-async def wait_random(max_delay: int 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
-    funcion asincrónica
+    Function asynchronous
     """
-    num: float = random.uniform(0, max_delay)
+    num = random.uniform(0, max_delay)
     await asyncio.sleep(num)
     return num
