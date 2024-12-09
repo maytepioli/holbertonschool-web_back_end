4b321badedcfe6e64a755ae4ad91cbc6e9129df1
maytepioli <maytepioli@gmail.com>
2024-12-09 14:17:28 -0300

    Added asynchronous generator that produces floating random numbers between 0 and 10 every second.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
+"""
+Este script contiene un generador asíncrono que produce
+números aleatorios flotantes
+entre 0 y 10 cada segundo. El generador usa `asyncio`
+para hacer las pausas de manera
+asíncrona, lo que permite que otros procesos
+continúen ejecutándose mientras se espera.
+"""
 import random
 import asyncio
 
+
 async def async_generator():
+    """
+    Este script contiene un generador asíncrono que produce
+    números aleatorios flotantes
+    entre 0 y 10 cada segundo. El generador usa `asyncio`
+    para hacer las pausas de manera
+    asíncrona, lo que permite que otros procesos
+    continúen ejecutándose mientras se espera.
+    """
     for i in range(11):
         await asyncio.sleep(1)
         num: float = random.uniform(0, 10)
