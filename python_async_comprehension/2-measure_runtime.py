b3f3804b7a2eb6b0099c2c56ad36804271db590d
maytepioli <maytepioli@gmail.com>
2024-12-10 13:18:59 -0300

    Measure execution time of async_comprehension using custom variable names for time

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+ Ejecuta la función async_comprehension cuatro veces en paralelo
+ y mide el tiempo total.
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    Ejecuta la función async_comprehension cuatro veces en paralelo
+    y mide el tiempo total.
+    """
+    tiempo_inicio = time.perf_counter()
+    tasks = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*tasks)
+    tiempo_final = time.perf_counter()
+    tiempo_total = tiempo_final - tiempo_inicio
+    return tiempo_total
