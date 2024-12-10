06661cdf211cd50ddb01d560a6e0a32f81367b4f
maytepioli <maytepioli@gmail.com>
2024-12-10 12:56:59 -0300

    implements the async_comprehension function to collect asynchronous numbers

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,11 @@ que usa un generador asíncrono
 para obtener una lista de números aleatorios.
 """
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Este archivo define la función async_comprehension
     que usa un generador asíncrono
